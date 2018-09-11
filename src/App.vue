fab3ccc78e19688062a4a20eec8e8edd667ce62d
Hannes Alm Admin <hanalmadmin@CONKNA069.local>
2018-09-11 10:51:51 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,142 +1,102 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer" fixed app>
-      <v-list dense>
-        <template v-for="item in items">
-          <v-layout v-if="item.heading" :key="item.heading" row align-center>
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-            <v-flex xs6 class="text-xs-center">
-              <a href="#!" class="body-2 black--text">EDIT</a>
-            </v-flex>
-          </v-layout>
-          <v-list-group v-else-if="item.children" v-model="item.model" :key="item.text" :prepend-icon="item.model ? item.icon : item['icon-alt']" append-icon="">
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile v-for="(child, i) in item.children" :key="i">
-              <v-list-tile-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ child.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-group>
-          <v-list-tile v-else :key="item.text">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="blue darken-3" dark app fixed>
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="hidden-sm-and-down">Mimerslunden</span>
-      </v-toolbar-title>
-      <v-text-field flat solo-inverted hide-details prepend-inner-icon="search" label="Search" class="hidden-sm-and-down"></v-text-field>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>notifications</v-icon>
-      </v-btn>
-      <v-btn icon large>
-        <v-avatar size="32px" tile>
-          <img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify">
-        </v-avatar>
-      </v-btn>
-    </v-toolbar>
     <v-content>
-      <v-container fluid fill-height>
-        <v-layout justify-center align-center>
-          <v-tooltip right>
-            <v-btn slot="activator" :href="source" icon large target="_blank">
-              <v-icon large>code</v-icon>
-            </v-btn>
-            <span>Source</span>
-          </v-tooltip>
-          <v-tooltip right>
-            <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/EQOYVV" target="_blank">
-              <v-icon large>mdi-codepen</v-icon>
-            </v-btn>
-            <span>Codepen</span>
-          </v-tooltip>
-        </v-layout>
-      </v-container>
-    </v-content>
-    <v-btn fab bottom right color="pink" dark fixed @click="dialog = !dialog">
-      <v-icon>add</v-icon>
-    </v-btn>
-    <v-dialog v-model="dialog" width="800px">
+      <v-toolbar color="indigo" dark fixed app>
+        <v-toolbar-title>Mimerslunden</v-toolbar-title>
+      </v-toolbar>
+      <v-alert :value="! landscape" color="error" transition="fade-transition">
+        Listan ses bäst om du har mobilen i liggande läge
+      </v-alert>
       <v-card>
-        <v-card-title class="grey lighten-4 py-4 title">
-          Create contact
-        </v-card-title>
-        <v-container grid-list-sm class="pa-4">
-          <v-layout row wrap>
-            <v-flex xs12 align-center justify-space-between>
-              <v-layout align-center>
-                <v-avatar size="40px" class="mr-3">
-                  <img src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png" alt="">
-                </v-avatar>
-                <v-text-field placeholder="Name"></v-text-field>
-              </v-layout>
-            </v-flex>
-            <v-flex xs6>
-              <v-text-field prepend-icon="business" placeholder="Company"></v-text-field>
-            </v-flex>
-            <v-flex xs6>
-              <v-text-field placeholder="Job title"></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field prepend-icon="mail" placeholder="Email"></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field type="tel" prepend-icon="phone" placeholder="(000) 000 - 0000" mask="phone"></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field prepend-icon="notes" placeholder="Notes"></v-text-field>
-            </v-flex>
-          </v-layout>
-        </v-container>
-        <v-card-actions>
-          <v-btn flat color="primary">More</v-btn>
+        <v-card-title>
+          Inbetalningar
           <v-spacer></v-spacer>
-          <v-btn flat color="primary" @click="dialog = false">Cancel</v-btn>
-          <v-btn flat @click="dialog = false">Save</v-btn>
-        </v-card-actions>
+          <v-text-field v-model="search" append-icon="search" label="Sök" single-line hide-details></v-text-field>
+        </v-card-title>
+        <v-data-table :headers="headers" :items="desserts" :search="search" rows-per-page-items>
+          <template slot="items" slot-scope="props">
+            <td>{{ props.item.name }}</td>
+            <td class="text-xs-left">{{ props.item.address }}</td>
+            <td class="text-xs-left">{{ props.item.amount }} kr</td>
+            <td class="text-xs-left">{{ props.item.date }}</td>
+          </template>
+          <v-alert slot="no-results" :value="true" color="error" icon="warning">
+            Din sökning på "{{ search }}" gav inga resultat.
+          </v-alert>
+        </v-data-table>
       </v-card>
-    </v-dialog>
+    </v-content>
   </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
-    dialog: false,
     drawer: null,
-    items: [{ icon: "money", text: "Inbetalningar", to: "payments" }]
+    landscape: false,
+    search: "",
+    headers: [
+      {
+        text: "Namn",
+        align: "left",
+        sortable: false,
+        value: "name"
+      },
+      { text: "Adress", value: "address", align: "left" },
+      { text: "Belopp", value: "amount", align: "left" },
+      { text: "Inbetalningsdatum", value: "date", align: "left" }
+    ],
+    desserts: [
+      {
+        value: false,
+        name: "Hannes och Anna",
+        address: "Skolgatan 7",
+        amount: 50,
+        date: "2018-01-01"
+      },
+      {
+        value: false,
+        name: "Tobbe och Cissi",
+        address: "Skolgatan 16",
+        amount: 50,
+        date: "2018-01-02"
+      }
+    ]
   }),
+
   props: {
     source: String
+  },
+
+  methods: {
+    list() {},
+
+    applyOrientation() {
+      if (window.innerHeight > window.innerWidth) {
+        this.landscape = false;
+      } else {
+        this.landscape = true;
+      }
+    }
+  },
+
+  mounted() {
+    const orientation =
+      screen.msOrientation ||
+      (screen.orientation || screen.mozOrientation || {}).type;
+
+    if (
+      orientation === "portrait-secondary" ||
+      orientation === "portrait-primary"
+    ) {
+      this.landscape = false;
+    } else {
+      this.landscape = true;
+    }
+
+    window.onresize = event => {
+      this.applyOrientation();
+    };
   }
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,41 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+  <v-card flat>
+    <v-toolbar color="primary" dark extended flat>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+    </v-toolbar>
+
+    <v-layout row pb-2>
+      <v-flex xs8 offset-xs2>
+        <v-card class="card--flex-toolbar">
+          <v-toolbar card prominent>
+            <v-toolbar-title class="body-2 grey--text">Title</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+              <v-icon>search</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>apps</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+          </v-toolbar>
+
+          <v-divider></v-divider>
+
+          <v-card-text style="height: 200px;"></v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+export default {};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
